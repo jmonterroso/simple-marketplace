adb09276dea121a1b5116b3ac78dd48fa58a4a7e
Jayson Monterroso <1420938+jmonterroso@users.noreply.github.com>
2021-11-24 13:29:05 -0600

    task: adding config files

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'hsl(267, 91%, 56%)',
+    },
+    secondary: {
+      main: 'hsl(45, 15%, 89%)',
+    },
+  },
+  typography: {
+    fontSize: 14,
+  },
+});
