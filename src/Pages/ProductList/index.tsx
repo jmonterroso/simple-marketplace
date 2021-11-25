42e74ffcc200d782575b785db5697e3f8aaa4656
Jayson Monterroso <1420938+jmonterroso@users.noreply.github.com>
2021-11-24 23:53:03 -0600

    adding search products functionality

--- src/Pages/ProductList/index.tsx
+++ src/Pages/ProductList/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Grid, Snackbar } from '@mui/material';
+import { Alert, Grid, IconButton, InputBase, Paper, Snackbar } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import * as Style from './style';
 import { fetchProducts } from '../../api';
@@ -8,6 +8,7 @@ import { bindActionCreators } from 'redux';
 import { actionCreators } from '../../state/actions';
 import { RootState } from '../../state/reducers';
 import { ICart } from '../../state/reducers/cartReducer';
+import SearchIcon from '@mui/icons-material/Search';
 
 const ProductList: React.FC = () => {
   const [products, setProducts] = useState<IProduct[]>([]);
@@ -18,11 +19,11 @@ const ProductList: React.FC = () => {
   const dispatch = useDispatch();
   const { cart }: ICart = useSelector((state: RootState) => state.cart);
   const { addToCart, updateCart } = bindActionCreators(actionCreators, dispatch);
+  const fetchData = async () => {
+    const productsFetched = await fetchProducts();
+    setProducts(productsFetched);
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      const productsFetched = await fetchProducts();
-      setProducts(productsFetched);
-    };
     fetchData();
   }, []);
   const handleAddToCart = (product: IProduct) => {
@@ -44,8 +45,30 @@ const ProductList: React.FC = () => {
       }
     }
   };
+
+  const searchProducts = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = event.target.value;
+    if (searchTerm) {
+      const filteredProducts = products.filter((product) => {
+        return product.name.toLowerCase().includes(searchTerm.toLowerCase());
+      });
+      setProducts(filteredProducts);
+    } else {
+      fetchData();
+    }
+  };
   return (
     <Style.Wrapper>
+      <Grid container mt={4} lg={12}>
+        <Grid item width={'100%'}>
+          <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%' }}>
+            <InputBase onChange={searchProducts} fullWidth={true} sx={{ ml: 1, mr: 1, flex: 1 }} placeholder="Search" />
+            <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+          </Paper>
+        </Grid>
+      </Grid>
       <Grid container spacing={2} mt={5}>
         {products.map((product, idx) => (
           <Grid item xs={12} sm={4} key={idx} mb={5} justifyContent={'center'} alignItems={'center'}>
